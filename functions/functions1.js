715e851c5c3b8844504ef7aa1622192de88634c9
storckmota <storckjuliocesar@gmail.com>
2022-07-20 21:30:20 -0300

    adicionando functions e scripts

--- functions/functions1.js
+++ functions/functions1.js
@@ -7,5 +7,76 @@ const sum = function(number1, number2){
 
 }
 
-
 sum(2,3) //arguments - argumentos
+
+
+//function scope 
+let subject = 'create video'
+
+function createThink(subject){
+    subject = 'study'
+    return subject 
+}
+
+console.log(crateThink(subject))
+console.log(subject)
+
+
+//function hoisting 
+
+sayMyName()
+
+function sayMyName(){
+    console.log('Julio')
+}
+
+//Arrow function 
+
+ const sayMyName = (name) => {
+    console.log(name)
+ }
+
+ sayMyName('Julio')
+
+ //Callback Function 
+
+ function sayMyName(name) {
+    console.log('antes de executar uma callback')
+
+    name()
+
+    console.log('depois de executar uma callback')
+ }
+
+sayMyName(
+    () => {
+        console.log('estou em uma callback')
+    }
+)
+
+
+/* 
+    function() constructor 
+
+    *expressao new 
+    * criar um novo objeto
+    * this keyword
+  
+*/ 
+
+function Person(name){
+    this.name = name 
+    this.walk = function(){
+        return this.name + "esta andando"
+    }
+}
+
+const mayk = new Person("Mayk")
+const joao = new Person("joao")
+console.log(Mayk.walk())
+console.log(joao.walk())
+
+
+
+
+
---
+++ scripts.js
@@ -0,0 +1,39 @@
+/* 
+   Prototype 
+   *prototype-based language
+   * prototype chain 
+   * __proto__
+   
+*/
+
+/*
+   type conversion (typecasting) vs type coersion 
+
+   *alteracao de um tipo de dado para outro
+   
+*/
+
+console.log('9'+ 5)
+console.log(Nummber('9') + 5)
+
+//Manipulando Strings e Numeros 
+
+// Transformar String em Numero e Numero em String 
+
+let String = "123"
+console.log(Number(string))
+
+let number = 321
+console.log(String(number))
+
+//Manipulando Strings e Numeros 
+
+//Contar quantos caracteres tem uma palavra e quantos digitos tem um numero 
+
+let word = "Paralelepipedo"
+console.log(word.length)
+let number = 1234 
+console.log(String(number).length)
+
+//transformar um numero quebrado com 2 casas decimais e trocar ponto por virgula
+
